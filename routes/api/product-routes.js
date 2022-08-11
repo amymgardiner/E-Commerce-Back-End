0de0693b9948a3d96bf4cce5ed8ffd3eda742f9e
Amy Gardiner <amymgardiner@gmail.com>
2022-08-11 17:34:31 -0500

    product put route looked done, so I made the delete route and tested in insomnia so it works

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -130,14 +130,29 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Product.destroy();
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
