c3c3a863e5e1426fe3411a25c80f193bad9d5cea
Amy Gardiner <amymgardiner@gmail.com>
2022-08-11 16:09:21 -0500

    defined product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -18,27 +36,3 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
-
-// ProductTag
-
-// id
-
-// Integer
-
-// Doesn't allow null values
-
-// Set as primary key
-
-// Uses auto increment
-
-// product_id
-
-// Integer
-
-// References the product model's id
-
-// tag_id
-
-// Integer
-
-// References the tag model's id
